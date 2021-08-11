97a80ad50cbdffb37eece7feaac8699540d74625
FosseyCool <Verein33@gmail.com>
2021-08-11 16:11:07 +0300

    Work_3.0

---
+++ Timesheets/DAL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Timesheets.DAL.Interfaces;
+using Timesheets.Model;
+
+namespace Timesheets.DAL.Repositories
+{
+    public interface IEmployeeRepository:IRepository<Employee>
+    {
+        
+    }
+}
---
+++ Timesheets/Model/Invoices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Timesheets.Model
+{
+    public class Invoices
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public int ContractId { get; set; }
+        
+        [Required]
+        public int EmployeeId { get; set; }
+        
+        [Required]
+        public DateTimeOffset Date { get; set; }
+        
+        [Required]
+        public int Total { get; set; }
+    }
+}
